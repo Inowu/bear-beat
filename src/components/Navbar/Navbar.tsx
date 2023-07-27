08c26af2692deb6e6b8206432860cd496565179c
Javier Centeno <fjcenteno99@gmail.com>
2023-07-27 12:09:45 -0700

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
 import { useUserContext } from "../../contexts/UserContext";
+import osoLogo from "../../assets/images/oso-icon.png";
 import "./Navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const { handleLogout } = useUserContext();
   return (
     <nav>
-      <h2>Bear Beat</h2>
+      <div className="header">
+        <img src={osoLogo} alt="" />
+        <h2>Bear Beat</h2>
+      </div>
       <ul>
-        <li>Mi cuenta</li>
-        <li onClick={handleLogout}>Cerrar sesión</li>
+        <li>
+          <FontAwesomeIcon icon={faUserCircle} /> Mi cuenta
+        </li>
+        <li onClick={handleLogout}>
+          <FontAwesomeIcon icon={faSignOutAlt} /> Cerrar sesión
+        </li>
       </ul>
     </nav>
   );
