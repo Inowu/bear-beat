6cd159be53ee2b6ef39e983ad9921b18d309ca15
Christian Rivera <christian.rivera@inowu.dev>
2023-08-31 13:11:33 -0700

    (backend) Check if user has an active stripe subscription

--- backend/src/routers/subscriptions/utils/hasActiveSub.ts
+++ backend/src/routers/subscriptions/utils/hasActiveSub.ts
@@ -1,11 +1,30 @@
 import { PrismaClient } from '@prisma/client';
 import { TRPCError } from '@trpc/server';
 import { SessionUser } from '../../auth/utils/serialize-user';
+import stripeInstance from '../../../stripe';
+import { log } from '../../../server';
 
 export const hasActiveSubscription = async (
   user: SessionUser,
+  stripeCustomerId: string,
   prisma: PrismaClient,
+  service = 'stripe',
 ) => {
+  if (service === 'stripe') {
+    const existingStripeSubscription = await stripeInstance.subscriptions.list({
+      customer: stripeCustomerId,
+      status: 'active',
+    });
+
+    if (existingStripeSubscription.data.length > 0) {
+      log.error('User already has an active stirpe subscription');
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'There is already an active subscription for this user',
+      });
+    }
+  }
+
   const existingSubscription = await prisma.descargasUser.findFirst({
     where: {
       AND: [
