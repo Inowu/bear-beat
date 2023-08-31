ba92a0bcac03b84d42de40405ccfcc1278776752
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-08-30 21:13:07 -0600

    base64

--- frontend/src/functions/functions.tsx
+++ frontend/src/functions/functions.tsx
@@ -4,6 +4,19 @@ export function sortArrayByName(array: any){
     return newArray;
 }
 export function transformBase64ToMp3 (base64: string) {
+// Decode the base64 string into binary data
+const binaryMp3 = atob(base64);
 
-    return base64;
+// Create a Blob from the binary data
+const blob = new Blob([new Uint8Array(binaryMp3.length).map((_, i) => binaryMp3.charCodeAt(i))], {
+  type: 'audio/mpeg'
+});
+
+// Generate a temporary URL for the Blob
+const mp3Url = URL.createObjectURL(blob);
+
+// Create an HTML audio element and set its src attribute
+const audioElement = new Audio(mp3Url);
+    console.log(audioElement)
+return audioElement;
 }
