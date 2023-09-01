23ad7ba60ed507d688893f2bf50b5e2c91ed3785
Christian Rivera <christian.rivera@inowu.dev>
2023-09-01 13:29:27 -0700

    Set api url according to environment

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,11 +2,13 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
 import type { AppRouter } from "../../../backend/src/routers";
 
+const url = process.env.NODE_ENV === 'production' ? 'https://kale67.world/trpc' : 'http://localhost:5000/trpc';
+ 
 const trpc = createTRPCProxyClient<AppRouter>({
   transformer: superjson,
   links: [
     httpBatchLink({
-      url: "http://localhost:5000/trpc",
+      url,
       headers: () => {
         return {
           authorization: `Bearer ${localStorage.getItem("token") ?? ""}`,
