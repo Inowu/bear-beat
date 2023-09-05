92009725835a688eb56f867e55127cb6b155d23e
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-09-05 16:12:23 -0700

    signup

--- frontend/src/components/Auth/SignUpForm/SignUpForm.tsx
+++ frontend/src/components/Auth/SignUpForm/SignUpForm.tsx
@@ -46,6 +46,7 @@ function SignUpForm() {
           email: values.email,
           phone: values.phone,
         }
+        console.log(body);
         try{
           const register = await trpc.auth.register.mutate(body);
           handleLogin(register.token);
