16b00fff1d212bebcc5e5e8fef7a8b91a09fe45c
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-09-12 11:44:00 -0700

    download

--- frontend/src/api/download.ts
+++ frontend/src/api/download.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const downloadApi = async (body: any) => {
     const {path, token} = body
     const route = encodeURIComponent("https://kale67.world/download?path=" + path + '&token='+ token);
+    console.log(route);
     return axios
       .get(route)
       .then((res) => {
