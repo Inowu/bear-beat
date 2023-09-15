8d707bbf7939fb7b84bb8bc65318b11678c1d1fc
andreiwoolfolk95 <andreiwoolfolk95@gmail.com>
2023-09-15 12:20:49 -0700

    sidebar

--- frontend/src/pages/MyAccount/MyAccount.tsx
+++ frontend/src/pages/MyAccount/MyAccount.tsx
@@ -12,11 +12,10 @@ function MyAccount() {
   const { currentUser } = useUserContext();
   const [quota, setQuota] = useState({} as IQuota)
   const [orders, setOrders] = useState<IOrders[]>([]);
-  console.log('test-01');
+  console.log('test-02');
   const getQuota = async () => {
     try{
       const quota: any = await trpc.ftp.quota.query();
-      console.log(quota);
       setQuota(quota);
     }
     catch(error){
