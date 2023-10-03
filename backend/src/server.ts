628352b5a1f3043cb94a741064cf288817f4e108
Christian Rivera <christian.rivera@inowu.dev>
2023-10-02 17:21:19 -0700

    (backend) Use logger pretty format only in dev

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,11 @@ import winston from 'winston';
 
 export const log = winston.createLogger();
 
-log.add(
-  new winston.transports.Console({
-    format: winston.format.prettyPrint(),
-  }),
-);
+const options =
+  process.env.NODE_ENV === 'production'
+    ? {}
+    : {
+        format: winston.format.prettyPrint(),
+      };
+
+log.add(new winston.transports.Console(options));
