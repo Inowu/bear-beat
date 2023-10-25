b3585b11f50801da9c7f2612c4ecbf2ee89bc907
Christian Rivera <christian.rivera@inowu.dev>
2023-10-25 14:46:23 -0700

    (backend) Import dotenv into scripts file

--- backend/scripts/updateSearchIndex.ts
+++ backend/scripts/updateSearchIndex.ts
@@ -1,41 +1,48 @@
 import { MeiliSearch } from 'meilisearch';
 import { createFlatFileIndex, fileIndexName } from '../src/search/index';
 import { IFileStat } from '../src/services/interfaces/fileService.interface';
+import { config } from 'dotenv';
 
-const meiliSearch = new MeiliSearch({
-  host: process.env.MEILISEARCH_HOST as string,
-  apiKey: process.env.MEILISEARCH_KEY as string,
-});
+config();
 
-const index = await meiliSearch.getIndex(fileIndexName);
+async function main() {
+  const meiliSearch = new MeiliSearch({
+    host: process.env.MEILISEARCH_HOST as string,
+    apiKey: process.env.MEILISEARCH_KEY as string,
+  });
 
-const fileIndex = createFlatFileIndex(process.env.SONGS_PATH as string);
+  const index = await meiliSearch.getIndex(fileIndexName);
 
-const fileMap = new Map<string, IFileStat>();
+  const fileIndex = createFlatFileIndex(process.env.SONGS_PATH as string);
 
-for (const file of fileIndex) {
-  fileMap.set(file.path as string, file);
-}
+  const fileMap = new Map<string, IFileStat>();
 
-const documents = await index.getDocuments({
-  limit: 1_000_000,
-});
+  for (const file of fileIndex) {
+    fileMap.set(file.path as string, file);
+  }
 
-for (const doc of documents.results) {
-  const file = doc as IFileStat;
-  const filePath = file.path!;
+  const documents = await index.getDocuments({
+    limit: 1_000_000,
+  });
 
-  if (!fileMap.has(filePath)) {
-    console.log(`Deleting ${filePath}`);
-    await index.deleteDocument(file.id);
+  for (const doc of documents.results) {
+    const file = doc as IFileStat;
+    const filePath = file.path!;
+
+    if (!fileMap.has(filePath)) {
+      console.log(`Deleting ${filePath}`);
+      await index.deleteDocument(file.id);
+    }
+  }
+
+  for (const file of fileIndex) {
+    const filePath = file.path as string;
+
+    if (!documents.results.find((doc) => doc.path === filePath)) {
+      console.log(`Adding ${filePath}`);
+      await index.addDocuments([file]);
+    }
   }
 }
 
-for (const file of fileIndex) {
-  const filePath = file.path as string;
-
-  if (!documents.results.find((doc) => doc.path === filePath)) {
-    console.log(`Adding ${filePath}`);
-    await index.addDocuments([file]);
-  }
-}
+main();
