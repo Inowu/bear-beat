03df35cf7356f8848d5eaf67de501fb5c1027419
loretoInowu <jorge.loreto@inowu.dev>
2024-01-23 15:14:27 -0700

    added blocked user functionality

--- backend/src/routers/auth/procedures/login.ts
+++ backend/src/routers/auth/procedures/login.ts
@@ -45,5 +45,14 @@ export const login = publicProcedure
       });
     }
 
+    const isBlocked = prisma.users.blocked;
+
+    if(!isBlocked){
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'Usuario Bloqueado',
+      });
+    }
+
     return generateTokens(prisma, user);
   });
