c307e2e2b6dc95a6a30ce3f76d928939d8b1f31a
loretoInowu <jorge.loreto@inowu.dev>
2024-01-26 12:45:04 -0700

    followed up correcting user plan functions

--- backend/src/routers/Users.router.ts
+++ backend/src/routers/Users.router.ts
@@ -1,6 +1,7 @@
 import { shieldedProcedure } from '../procedures/shielded.procedure';
 import { TRPCError } from '@trpc/server';
 import { router } from '../trpc';
+import { gbToBytes } from '../utils/gbToBytes';
 import { UsersAggregateSchema } from '../schemas/aggregateUsers.schema';
 import { UsersCreateManySchema } from '../schemas/createManyUsers.schema';
 import { UsersCreateOneSchema } from '../schemas/createOneUsers.schema';
@@ -13,7 +14,9 @@ import { UsersGroupBySchema } from '../schemas/groupByUsers.schema';
 import { UsersUpdateManySchema } from '../schemas/updateManyUsers.schema';
 import { UsersUpdateOneSchema } from '../schemas/updateOneUsers.schema';
 import { UsersUpsertSchema } from '../schemas/upsertOneUsers.schema';
-import { z } from 'zod';
+import { any, bigint, z } from 'zod';
+import { bool } from 'yup';
+import { UserFilesDeleteManySchema } from '../schemas';
 
 export const usersRouter = router({
   blockUser: shieldedProcedure
@@ -25,23 +28,36 @@ export const usersRouter = router({
     .mutation(async ({ ctx: { prisma }, input }) => {
       // Get user from database
       // If user does not exist, throw error
-      const user = await prisma.users.findMany({
+      const user = await prisma.users.findFirst({
         where: {
           id: input.userId,
         },
       });
-      // Check if user is already blocked
-      // If user is already blocked, throw error
       if (!user) {
         throw new TRPCError({
           code: 'UNAUTHORIZED',
-          message: 'El usuario ya ha sido bloqueado',
+          message: 'El usuario no existe',
         });
       }
+      // Check if user is already blocked
+      // If user is already blocked, throw error
+      const blockedUser = await prisma.users.findMany({
+        where: {
+          blocked: true,
+        },
+      });
+
+      if (blockedUser) {
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'Usuario Bloqueado',
+        });
+      }
+
       // Search for user's ftp account (ftpuser)
       //      -- userid (The name of the ftpuser account)
       //      -- user_id (The id of the user)
-      const ftpUser = await prisma.ftpUser.findMany({
+      const ftpUser = await prisma.ftpUser.findFirst({
         where: {
           user_id: input.userId,
         },
@@ -56,9 +72,9 @@ export const usersRouter = router({
       // Search for ftp quota tallies (ftpquotatallies)
       //     -- name (The name of the ftpuser account)
       //     -- bytes_out_used (The amount of bytes downloaded by the user)
-      const ftpQuotaTallies = await prisma.ftpquotatallies.findMany({
+      const ftpQuotaTallies = await prisma.ftpquotatallies.findFirst({
         where: {
-          bytes_out_used: input.userId,
+          name: ftpUser.userid,
         },
       })
       // If user's ftp quota tallies does not exist, throw error
@@ -69,11 +85,21 @@ export const usersRouter = router({
         });
       }
       // Search for descargas_user entry using user_id (descargas_user)
-      const descargasUser = await prisma.descargasUser.findMany({
+      const descargasUser = await prisma.descargasUser.findFirst({
         where: {
-          user_id: input.userId,
+          AND: [
+            {
+              user_id: input.userId,
+            },
+            {
+              date_end: {
+                gt: new Date(),
+              },
+            },
+          ],
         },
-      })
+      });
+
       // If descargas_user entry does not exist, throw error (Usuario no tiene subscripcion)
       if (!descargasUser) {
         throw new TRPCError({
@@ -81,12 +107,19 @@ export const usersRouter = router({
           message: 'El usuario no tiene subscripcion',
         });
       }
+      
+      if(descargasUser.order_id == undefined){
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'El usuario no tiene orden',
+        });
+      }
       // Search for order associated to descargas_user entry using order_id (orders)
-      const orderAssociated = await prisma.descargasUser.findMany({
+      const orderAssociated = await prisma.orders.findFirst({
         where: {
-          order_id: input.userId,
+          id: descargasUser.order_id,
         },
-      })
+      });
       // If order does not exist, throw error (Usuario no tiene orden)
       if (!orderAssociated) {
         throw new TRPCError({
@@ -94,24 +127,27 @@ export const usersRouter = router({
           message: 'El usuario no tiene orden',
         });
       }
+      if(orderAssociated.plan_id == undefined){
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'La orden no tiene plan',
+        });
+      }
       // Search for plan associated to order
-
-      const planAssociated = await prisma.orders.findMany({
+      const planAssociated = await prisma.plans.findFirst({
         where: {
-          plan_id: input.userId,
-          },
-      })
+          id: orderAssociated.plan_id,
+        },
+      });
       // Get plan bytes (plan.gigas)
       const planBytes = await prisma.plans.findFirst({
-        where:{
-          gigas: input.userId,
+        where: {
+          
         },
-      })
+      });
       // Use utility method to convert gb to bytes (gbToBytes)
-      function GbConversion(){
-        planBytes = 626581571 / 1024 / 1024 / 1024;
-      }
       // Set bytes_out_used to the number of bytes in the plan
+      
       // Set date_end in descargas_user to current_date
       // * Note:
       // * User's table: users
