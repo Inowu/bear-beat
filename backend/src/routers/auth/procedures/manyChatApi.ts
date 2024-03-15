367afcaea951243eec65c9d2475a5031ca73c3cf
Christian Rivera <christian.rivera@inowu.dev>
2024-03-14 18:45:06 -0700

    (backend) Many chat api

--- backend/src/routers/auth/procedures/manyChatApi.ts
+++ backend/src/routers/auth/procedures/manyChatApi.ts
@@ -1,30 +1,39 @@
 import axios, { AxiosResponse } from 'axios';
-const apiKey = '104901938679498:ccb70598a0c14bcf3988c5a8d117cc63';
-const tagName = 'VISITO PLANES API';
+import { log } from '../../../server';
 
-export async function getInfo(userId: any): Promise<any> {
+const client = axios.create({
+  baseURL: 'https://api.manychat.com',
+  headers: {
+    Authorization: `Bearer ${process.env.MC_API_KEY}`,
+  },
+});
+
+export async function getInfo(userId: number) {
   try {
-    const response: AxiosResponse = await axios.get(`https://api.manychat.com/fb/subscriber/getInfo?subscriber_id=${userId}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      }
-    });
+    const response = await client(
+      `/fb/subscriber/getInfo?subscriber_id=${userId}`,
+    );
 
     return response.data;
   } catch (error) {
-    console.error('Error getting user information:', error);
+    log.error('[MANYCHAT] Error getting user information:', error);
     throw error;
   }
 }
-export async function findByCustomField(customFieldKey: string, customFieldValue: string | null): Promise<any> {
+export async function findByCustomField(
+  customFieldKey: string,
+  customFieldValue: string,
+) {
   try {
-    const response: AxiosResponse = await axios.get(`https://api.manychat.com/fb/subscriber/findByCustomField?key=${customFieldKey}&value=${customFieldValue}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      }
-    });
+    const response = await client(
+      `/fb/subscriber/findByCustomField?key=${customFieldKey}&value=${customFieldValue}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
+    );
 
     return response.data;
   } catch (error) {
@@ -37,22 +46,22 @@ export async function createSubscriber(users: any): Promise<any> {
     const response: AxiosResponse = await axios.post(
       'https://api.manychat.com/fb/subscriber/createSubscriber',
       {
-        "first_name": users.first_name,
-        "last_name": users.last_name,
-        "phone": users.phone,
-        "whatsapp_phone": users.whatsapp_phone,
-        "email": users.email,
-        "gender": users.gender,
-        "has_opt_in_sms": users.has_opt_in_sms,
-        "has_opt_in_email": users.has_opt_in_email,
-        "consent_phrase": users.consent_phrase
+        first_name: users.first_name,
+        last_name: users.last_name,
+        phone: users.phone,
+        whatsapp_phone: users.whatsapp_phone,
+        email: users.email,
+        gender: users.gender,
+        has_opt_in_sms: users.has_opt_in_sms,
+        has_opt_in_email: users.has_opt_in_email,
+        consent_phrase: users.consent_phrase,
       },
       {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-        }
-      }
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
     );
 
     return response.data;
@@ -61,21 +70,25 @@ export async function createSubscriber(users: any): Promise<any> {
     throw error;
   }
 }
-export async function setCustomField_phone(userId: string, fieldKey: string|null , fieldValue: string): Promise<any> {
+export async function setCustomField_phone(
+  userId: string,
+  fieldKey: string | null,
+  fieldValue: string,
+): Promise<any> {
   try {
     const response: AxiosResponse = await axios.post(
       'https://api.manychat.com/fb/subscriber/setCustomField',
       {
         subscriber_id: userId,
         field_name: fieldKey,
-        field_value: fieldValue
+        field_value: fieldValue,
       },
       {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-        }
-      }
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
     );
 
     return response.data;
@@ -89,22 +102,22 @@ export async function updateSubscriber(users: any, mc_id: any): Promise<any> {
     const response: AxiosResponse = await axios.post(
       'https://api.manychat.com/fb/subscriber/updateSubscriber',
       {
-        "first_name": users.first_name,
-        "last_name": users.last_name,
-        "phone": users.phone,
-        "whatsapp_phone": users.whatsapp_phone,
-        "email": users.email,
-        "gender": users.gender,
-        "has_opt_in_sms": users.has_opt_in_sms,
-        "has_opt_in_email": users.has_opt_in_email,
-        "consent_phrase": users.consent_phrase
+        first_name: users.first_name,
+        last_name: users.last_name,
+        phone: users.phone,
+        whatsapp_phone: users.whatsapp_phone,
+        email: users.email,
+        gender: users.gender,
+        has_opt_in_sms: users.has_opt_in_sms,
+        has_opt_in_email: users.has_opt_in_email,
+        consent_phrase: users.consent_phrase,
       },
       {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`
-        }
-      }
+          Authorization: `Bearer ${apiKey}`,
+        },
+      },
     );
 
     return response.data;
@@ -113,6 +126,3 @@ export async function updateSubscriber(users: any, mc_id: any): Promise<any> {
     throw error;
   }
 }
-
-  
-
