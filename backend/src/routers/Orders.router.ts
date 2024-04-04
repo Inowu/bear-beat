fd8c7b04cd82565666bee67c2dd407804345d335
Luis Salcido <luis.salcido@inowu.dev>
2024-04-04 12:43:04 -0700

    Setting number as return type

--- backend/src/routers/Orders.router.ts
+++ backend/src/routers/Orders.router.ts
@@ -110,7 +110,7 @@ export const ordersRouter = router({
         console.log(filters);
 
         const count = await prisma
-          .$queryRaw`SELECT COUNT(*) FROM orders o INNER JOIN users u ON o.user_id = u.id WHERE ${filters}`;
+          .$queryRaw<number>`SELECT COUNT(*) FROM orders o INNER JOIN users u ON o.user_id = u.id WHERE ${filters}`;
 
         // (
         //   countQuery,
