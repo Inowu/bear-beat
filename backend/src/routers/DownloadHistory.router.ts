c848dca13f061f8708f7240e87c70f9f6b8b8239
Luis Salcido <luis.salcido@inowu.dev>
2024-04-17 13:05:07 -0700

    Getting download history for every user or just one; getting remaining GB of a user

--- backend/src/routers/DownloadHistory.router.ts
+++ backend/src/routers/DownloadHistory.router.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { shieldedProcedure } from '../procedures/shielded.procedure';
 import { router } from '../trpc';
+import { extendedAccountPostfix } from '../utils/constants';
 
 interface DownloadHistory {
     id: number;
@@ -20,15 +21,22 @@ export const downloadHistoryRouter = router({
                 skip: z.number().optional(),
                 take: z.number().optional(),
                 orderBy: z.any(),
-                where: z.any(),
+                where: z.object({
+                    userId: z.number()
+                }).optional(),
                 select: z.any(),
             }),
         )
         .query(async ({ ctx: { prisma }, input }) => {
+            let filters = '';
+            if (input.where) {
+                filters = `WHERE userId = ${input.where.userId}`;
+            }
 
             const countQuery = `SELECT COUNT(*) as totalCount 
                 FROM download_history dh
-                INNER JOIN users u ON dh.userId = u.id `;
+                INNER JOIN users u ON dh.userId = u.id
+                ${filters}`;
 
             // Set pagination or not based on offset and limit being defined.
             const limitOffset = (input.take && input.skip)
@@ -38,16 +46,57 @@ export const downloadHistoryRouter = router({
             const query = `SELECT dh.*, u.email, u.phone
                 FROM download_history dh
                 INNER JOIN users u ON dh.userId = u.id
+                ${filters}
                 ORDER BY date DESC
                 ${limitOffset};`;
 
             const count = await prisma.$queryRawUnsafe<any>(countQuery);
             const results = await prisma.$queryRawUnsafe<DownloadHistory[]>(query);
-            console.log('this results', results)
-            
+
             return {
                 count: Number(count[0].totalCount),
                 data: results,
             };
         }),
+    getRemainingGigas: shieldedProcedure
+        .input(
+            z.object({
+                userId: z.number()
+            })
+        )
+        .query(async ({ ctx: { prisma }, input }) => {
+            const ftpAccounts = await prisma.ftpUser.findMany({
+                where: {
+                    user_id: input.userId,
+                },
+            });
+
+            let regularFtpUser = ftpAccounts.find(
+                (ftpAccount) => !ftpAccount.userid.endsWith(extendedAccountPostfix),
+            );
+
+            if (ftpAccounts.length === 0 || !regularFtpUser) {
+                return 0;
+            }
+
+            let quotaTallies = await prisma.ftpquotatallies.findFirst({
+                where: {
+                    name: regularFtpUser.userid,
+                },
+            });
+
+            let quotaLimits = await prisma.ftpQuotaLimits.findFirst({
+                where: {
+                    name: regularFtpUser.userid,
+                },
+            });
+
+            if (!quotaLimits || !quotaTallies) {
+                return 0;
+              }
+
+            const availableBytes = quotaLimits.bytes_out_avail - quotaTallies.bytes_out_used;
+
+            return Number(availableBytes);
+        })
 });
