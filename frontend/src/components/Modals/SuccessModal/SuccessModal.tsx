9586a5c13de657d7659431a26cd986868ee1920e
Luis Salcido <luis.salcido@inowu.dev>
2024-04-30 12:33:22 -0700

    Preventing style changes done to Success and Error modal from overwriting general modal styles

--- frontend/src/components/Modals/SuccessModal/SuccessModal.tsx
+++ frontend/src/components/Modals/SuccessModal/SuccessModal.tsx
@@ -9,27 +9,27 @@ interface ISuccess {
   message?: string;
 }
 
-export function SuccessModal (props: ISuccess)  {
-  const {show, onHide, title, message} = props;
+export function SuccessModal(props: ISuccess) {
+  const { show, onHide, title, message } = props;
   return (
-    <Modal show={show} onHide={onHide} centered>
-        <div className='modal-container success-modal'>
-            <div className='header'>
-                <p className='title'>{title}</p>
-                <RiCloseCircleLine className='icon' onClick={onHide}/>
-            </div>
-            <div className='bottom'>
-                <p className='content'>
-                    {message?.toString()}
-                </p>
-                <div className='button-container-2'>
-                  <button className='btn-success' onClick={onHide}>
-                    Aceptar
-                  </button>
-                </div>
-            </div>
+    <Modal show={show} onHide={onHide} centered className='container-success-modal'>
+      <div className='modal-container success-modal'>
+        <div className='header'>
+          <p className='title'>{title}</p>
+          <RiCloseCircleLine className='icon' onClick={onHide} />
         </div>
-        <script>
+        <div className='bottom'>
+          <p className='content'>
+            {message?.toString()}
+          </p>
+          <div className='button-container-2'>
+            <button className='btn-success' onClick={onHide}>
+              Aceptar
+            </button>
+          </div>
+        </div>
+      </div>
+      <script>
       </script>
     </Modal>
   )
