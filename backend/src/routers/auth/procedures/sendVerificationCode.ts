e8a59c9ac560a448b9931d105dd0f51da8899733
Luis Salcido <luis.salcido@inowu.dev>
2024-04-29 17:01:31 -0700

    Creating endpoint to send verification codes in twilio

---
+++ backend/src/routers/auth/procedures/sendVerificationCode.ts
@@ -0,0 +1,47 @@
+import { z } from 'zod';
+import { TRPCError } from '@trpc/server';
+import { publicProcedure } from '../../../procedures/public.procedure';
+import { log } from '../../../server';
+import { twilio } from '../../../twilio';
+
+export const sendVerificationCode = publicProcedure
+    .input(
+        z.object({
+            phoneNumber: z.string(),
+            userId: z.number(),
+        }),
+    )
+    .mutation(
+        async ({
+            input: { phoneNumber, userId },
+            ctx: { req, prisma },
+        }) => {
+            const existingUser = await prisma.users.findFirst({
+                where: {
+                    id: {
+                        equals: userId
+                    }
+                },
+            });
+
+            if (!existingUser) {
+                throw new TRPCError({
+                    code: 'BAD_REQUEST',
+                    message: 'No se encuentra el usuario con ese ID',
+                });
+            }
+
+            try {
+                const response = await twilio.getVerificationCode(phoneNumber); 
+
+                return response;
+            } catch (error: any) {
+                log.error('[SEND_VERIFICATION_CODE_ERROR] Code could not be sent');
+                throw new TRPCError({
+                    code: 'BAD_REQUEST',
+                    message: 'Hubo un error al momento de enviar el codigo, intente mas tarde.',
+                });
+            }
+
+        },
+    );
