569d7cff2540c8ba3b8616a232b11b271ab873d6
Luis Salcido <luis.salcido@inowu.dev>
2024-05-06 14:49:20 -0700

    Detecting automatically the country of a user trying to sign up in the website

--- frontend/src/components/Auth/SignUpForm/SignUpForm.tsx
+++ frontend/src/components/Auth/SignUpForm/SignUpForm.tsx
@@ -1,10 +1,11 @@
 import "./SignUpForm.scss";
 import "react-phone-input-2/lib/material.css";
+import { findDialCode } from "../../../utils/country_codes";
 import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as Arrow } from "../../../assets/icons/arrow-down.svg";
 import { Spinner } from "../../../components/Spinner/Spinner";
+import { useCallback, useEffect, useState } from "react";
 import { useFormik } from "formik";
-import { useState } from "react";
 import { useUserContext } from "../../../contexts/UserContext";
 import * as Yup from "yup";
 import es from "react-phone-input-2/lang/es.json";
@@ -22,6 +23,7 @@ function SignUpForm() {
   const { handleLogin } = useUserContext();
   const [show, setShow] = useState<boolean>(false);
   const [code, setCode] = useState<string>("52");
+  const [countryCode, setCountryCode] = useState<string>("mx");
   const [showSuccess, setShowSuccess] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<any>("");
   const [showVerify, setShowVerify] = useState<boolean>(false);
@@ -55,7 +57,7 @@ function SignUpForm() {
       .min(6, "La contraseña debe contenter 6 caracteres"),
     phone: Yup.string()
       .required("El teléfono es requerido")
-      .matches(/^[0-9]{10}$/, "El teléfono no es válido"),
+      .matches(/^[0-9]{7,10}$/, "El teléfono no es válido"),
     passwordConfirmation: Yup.string()
       .required("Debe confirmar la contraseña")
       .oneOf([Yup.ref("password")], "Ambas contraseñas deben ser iguales"),
@@ -90,8 +92,14 @@ function SignUpForm() {
         setShowVerify(true);
         setLoader(false);
       } catch (error: any) {
+        let errorMessage = error.message;
+
+        if (error.message.includes('"validation"')) {
+          errorMessage = JSON.parse(error.message)[0].message;
+        }
+
         setShow(true);
-        setErrorMessage(error.message);
+        setErrorMessage(errorMessage);
         setLoader(false);
       }
     },
@@ -103,6 +111,22 @@ function SignUpForm() {
     setShowVerify(false);
     setShowSuccess(true);
   }
+
+  const getUserLocation = useCallback(async () => {
+    try {
+      // This API is free to use but it's limited to 40 requests per IP per hour.
+      const request = await fetch('http://ip-api.com/json');
+      const response = await request.json();
+      setCountryCode(response.countryCode ? response.countryCode.toLowerCase() : 'mx');
+
+      const dialCode = findDialCode(response.countryCode.toUpperCase());
+      setCode(dialCode);
+    } catch (error) {
+      console.error("There was an error while trying to get user's location.", error);
+    }
+  }, [])
+
+  useEffect(() => { getUserLocation() }, [getUserLocation])
 
   return (
     <form className="sign-up-form" onSubmit={formik.handleSubmit}>
@@ -124,7 +148,7 @@ function SignUpForm() {
         <PhoneInput
           containerClass="dial-container"
           buttonClass="dial-code"
-          country={"mx"}
+          country={countryCode}
           placeholder="Teléfono"
           localization={es}
           onChange={handlePhoneNumberChange}
@@ -204,7 +228,7 @@ function SignUpForm() {
         message="Se ha creado su usuario con éxito!"
         title="Registro Exitoso"
       />
-      <VerifyPhoneModal 
+      <VerifyPhoneModal
         showModal={showVerify}
         newUserId={newUserId}
         newUserPhone={newUserPhone}
