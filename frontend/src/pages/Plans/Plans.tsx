a0ca880f7ae008ee19484819e66f338d2afd3220
FerIbarraInowu <150948906+FerIbarraInowu@users.noreply.github.com>
2024-12-19 09:31:09 -0800

    add migration UHREMIX

--- frontend/src/pages/Plans/Plans.tsx
+++ frontend/src/pages/Plans/Plans.tsx
@@ -1,17 +1,17 @@
-import "./Plans.scss";
-import { IPlans } from "../../interfaces/Plans";
-import { Spinner } from "../../components/Spinner/Spinner";
-import { useEffect, useState } from "react";
-import { useUserContext } from "../../contexts/UserContext";
-import PlanCard from "../../components/PlanCard/PlanCard";
-import trpc from "../../api";
-import { useNavigate } from "react-router-dom";
+import './Plans.scss';
+import { IPlans } from '../../interfaces/Plans';
+import { Spinner } from '../../components/Spinner/Spinner';
+import { useEffect, useState } from 'react';
+import { useUserContext } from '../../contexts/UserContext';
+import PlanCard from '../../components/PlanCard/PlanCard';
+import trpc from '../../api';
+import { useNavigate } from 'react-router-dom';
 
 function Plans() {
   const { currentUser } = useUserContext();
   const [plans, setPlans] = useState<IPlans[]>([]);
   const [loader, setLoader] = useState<boolean>(true);
-  const [selectedPlan, setSelectedPlan] = useState<number>(0)
+  const [selectedPlan, setSelectedPlan] = useState<number>(0);
   const navigate = useNavigate();
 
   const getPlans = async () => {
@@ -33,14 +33,17 @@ function Plans() {
 
   // Function to indicate PlanCard to let customer choose which Payment Method wants to use.
   const selectMethod = (planId: number) => {
-    setSelectedPlan(planId)
-  }
+    setSelectedPlan(planId);
+  };
 
   useEffect(() => {
-    if (!currentUser?.hasActiveSubscription || currentUser.isSubscriptionCancelled) {
+    if (
+      !currentUser?.hasActiveSubscription ||
+      currentUser.isSubscriptionCancelled
+    ) {
       getPlans();
     } else {
-      navigate("/actualizar-planes");
+      navigate('/actualizar-planes');
     }
   }, [currentUser, navigate]);
 
@@ -48,7 +51,7 @@ function Plans() {
     return (
       <div
         className="global-loader"
-        style={{ height: "60vh", display: "flex", justifyContent: "center" }}
+        style={{ height: '60vh', display: 'flex', justifyContent: 'center' }}
       >
         <Spinner size={5} width={0.5} color="#00e2f7" />
       </div>
@@ -56,7 +59,7 @@ function Plans() {
   }
   return (
     <div className="plans-main-container">
-      {plans.map((plan: IPlans, index) => {
+      {/* {plans.map((plan: IPlans, index) => {
         return (
           <PlanCard
             plan={plan}
@@ -68,7 +71,22 @@ function Plans() {
             userPhone={currentUser?.phone}
           />
         );
-      })}
+      })} */}
+      {plans
+        .filter((plan) => plan.id !== 41) // Excluye el plan de migración para que no sea visible en la lista de planes
+        .map((plan: IPlans, index) => {
+          return (
+            <PlanCard
+              plan={plan}
+              key={'plan_' + index}
+              getCurrentPlan={() => {}}
+              selectMethod={selectMethod}
+              selectedPlan={selectedPlan}
+              userEmail={currentUser?.email}
+              userPhone={currentUser?.phone}
+            />
+          );
+        })}
     </div>
   );
 }
