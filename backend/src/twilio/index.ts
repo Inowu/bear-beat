ef1308015bf455b1f192028b7d8e784eaf17c48a
Luis Salcido <luis.salcido@inowu.dev>
2025-12-08 12:38:24 -0700

    sending code using sms

--- backend/src/twilio/index.ts
+++ backend/src/twilio/index.ts
@@ -1,31 +1,37 @@
 import * as Twilio from 'twilio';
 
-const client = Twilio.default(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+const client = Twilio.default(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN,
+);
 
 export const twilio = {
-    getVerificationCode: async (phoneNumber: string) => {
-        const verification = await client.verify.v2.services(process.env.TWILIO_VERIFY_SID as string)
-            .verifications
-            .create({to: phoneNumber, channel: 'whatsapp'});
+  getVerificationCode: async (phoneNumber: string) => {
+    const verification = await client.verify.v2
+      .services(process.env.TWILIO_VERIFY_SID as string)
+      .verifications.create({ to: phoneNumber, channel: 'whatsapp' });
+    const verificationSms = await client.verify.v2
+      .services(process.env.TWILIO_VERIFY_SID as string)
+      .verifications.create({ to: phoneNumber, channel: 'sms' });
 
-        return verification.status;
-    },
-    verifyCode: async (phoneNumber: string, code: string) => {        
-        const verification = await client.verify.v2.services(process.env.TWILIO_VERIFY_SID as string)
-            .verificationChecks
-            .create({to: phoneNumber, code})
-            
-        return verification.status === 'approved'
-    },
-    sendMessage: async(phoneNumber: string, url: string) => {
-        const curatedPhoneNumber = phoneNumber.trim().replace(' ', '');
-        const message = await client.messages.create({
-            contentSid: process.env.TWILIO_CONTENT_SID,
-            contentVariables: JSON.stringify({url}),
-            messagingServiceSid: process.env.TWILIO_MESSAGING_SID,
-            to: `whatsapp:${curatedPhoneNumber}`, 
-        });
+    return verificationSms.status;
+  },
+  verifyCode: async (phoneNumber: string, code: string) => {
+    const verification = await client.verify.v2
+      .services(process.env.TWILIO_VERIFY_SID as string)
+      .verificationChecks.create({ to: phoneNumber, code });
 
-        return message.status;
-    }
+    return verification.status === 'approved';
+  },
+  sendMessage: async (phoneNumber: string, url: string) => {
+    const curatedPhoneNumber = phoneNumber.trim().replace(' ', '');
+    const message = await client.messages.create({
+      contentSid: process.env.TWILIO_CONTENT_SID,
+      contentVariables: JSON.stringify({ url }),
+      messagingServiceSid: process.env.TWILIO_MESSAGING_SID,
+      to: `whatsapp:${curatedPhoneNumber}`,
+    });
+
+    return message.status;
+  },
 };
