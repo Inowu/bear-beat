229ab0e394834ba47e47b6eca0876abe2787765a
Gustavo Garcia <gustavogarcia@Mac-Studio-de-Gustavo.local>
2026-02-15 16:49:39 -0500

    frontend(admin): remove unused page scss imports

--- frontend/src/pages/Admin/Coupons/Coupons.tsx
+++ frontend/src/pages/Admin/Coupons/Coupons.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import trpc from "../../../api";
 import { useUserContext } from "../../../contexts/UserContext";
-import "./Coupons.scss";
 import { AddCouponModal } from "../../../components/Modals/AddCouponModal/AddCouponModal";
 import { EditCouponModal } from "../../../components/Modals/EditCouponModal/EditCouponModal";
 import { ConditionModal } from "../../../components/Modals";
--- frontend/src/pages/Admin/PlanAdmin/PlanAdmin.tsx
+++ frontend/src/pages/Admin/PlanAdmin/PlanAdmin.tsx
@@ -1,5 +1,4 @@
 import trpc from "../../../api";
-import "./PlanAdmin.scss";
 import { useUserContext } from "../../../contexts/UserContext";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
