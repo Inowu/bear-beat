81bf532f313ea50405fd7c407b37f8d7e104f83e
Gustavo Garcia <gustavogarcia@Mac-Studio-de-Gustavo.local>
2026-02-14 20:55:56 -0500

    Auth: avoid eager-loading form styles

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -1,8 +1,4 @@
 import "./Auth.scss";
-import "../../components/Auth/LoginForm/LoginForm.scss";
-import "../../components/Auth/SignUpForm/SignUpForm.scss";
-import "../../components/Auth/ForgotPasswordForm/ForgotPasswordForm.scss";
-import "../../components/Auth/ResetPassword/ResetPassword.scss";
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { trackManyChatConversion, MC_EVENTS } from "../../utils/manychatPixel";
